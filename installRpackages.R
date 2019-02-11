11a71fdb440e84159295cab32d067d4c848ea2f3
Mark Fernandes <mfernandes61@gmail.com>
2019-02-11 14:47:08 +0000

    Update installRpackages.R
    Put back dev_tools

--- installRpackages.R
+++ installRpackages.R
@@ -10,6 +10,6 @@ install.packages(c(
 "knitr"),
 repos="https://cran.ma.imperial.ac.uk/",quiet=TRUE)
 # Feb 2019 - Matt has removed the RCmdrMisc dependency from the Shiny Apps
-# install.packages("devtools")
-# require(devtools)
-# install_version("RcmdrMisc", version="2.5-0", repos="https://cloud.r-project.org")
+install.packages("devtools")
+require(devtools)
+install_version("RcmdrMisc", version="2.5-0", repos="https://cloud.r-project.org")
